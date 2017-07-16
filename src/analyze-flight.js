2c14f127ca5cffb4cb2b6a03713c911303a6cf69
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-16 21:25:00 +0200

    analyzeFlight: Calculate AAT turnpoints

--- src/analyze-flight.js
+++ src/analyze-flight.js
@@ -4,6 +4,7 @@ function analyzeFlight(flight, task) {
   let nextTP = 0;
 
   let start, finish;
+  let aatPoints = [];
 
   for (let i = 0; i < flight.length - 1; i++) {
     let fix1 = flight[i].coordinate;
@@ -30,11 +31,22 @@ function analyzeFlight(flight, task) {
     if (point) {
       nextTP += 1;
     }
+
+    if (nextTP > 1 && task.points[nextTP - 1].observationZone.isInside(fix1)) {
+      let _score =
+        turf.distance(fix1, task.points[nextTP - 2].location) +
+        turf.distance(fix1, task.points[nextTP].location);
+
+      let _lastScore = (aatPoints[nextTP - 1] && aatPoints[nextTP - 1]._score) || 0;
+      if (_score > _lastScore) {
+        aatPoints[nextTP - 1] = {_score, coordinate: fix1, i, secOfDay: flight[i].secOfDay};
+      }
+    }
   }
 
   let totalTime = finish.secOfDay - start.secOfDay;
 
-  return { start, finish, totalTime };
+  return { start, finish, totalTime, aatPoints };
 }
 
 module.exports = analyzeFlight;

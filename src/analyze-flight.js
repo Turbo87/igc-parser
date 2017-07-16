78e2f9f672fd1a25aa6b526199c79e9fe3019222
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-16 19:02:39 +0200

    analyzeFlight: Filter out line intersections in wrong direction

--- src/analyze-flight.js
+++ src/analyze-flight.js
@@ -15,11 +15,16 @@ function analyzeFlight(flight, task) {
   for (let i = 0; i < flight.length - 1; i++) {
     let fix1 = flight[i];
     let fix2 = flight[i + 1];
+
     let intersection = turf.lineIntersect(start_line, turf.lineString([fix1, fix2]));
-    if (intersection.features.length > 0) {
-      let point = intersection.features[0];
-      starts.push({ point, i });
-    }
+    if (intersection.features.length === 0) continue;
+
+    let bearing = turf.bearing(fix1, fix2);
+    let bearingDiff = turf.bearingToAngle(start_bearing - bearing);
+    if (bearingDiff > 90 && bearingDiff < 270) continue;
+
+    let point = intersection.features[0];
+    starts.push({point, i});
   }
 
   return { starts };

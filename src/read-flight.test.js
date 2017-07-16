9323067ca82162fc55e786ea2aa00340014783b6
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-16 18:27:30 +0200

    Add basic readFlight() function

---
+++ src/read-flight.test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const readFlight = require('./read-flight');
+
+describe('readFlight()', () => {
+  it(`reads "SW_77flqgg1" flight log correctly`, () => {
+    let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/SW_77flqgg1.igc`);
+    expect(flight).toMatchSnapshot();
+  })
+});

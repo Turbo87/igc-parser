97f715d627269e70f94a35fa25cf232424998d97
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-16 20:47:38 +0200

    Draw second flight

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const viewGeoJSON = require('./src/view-geojson');
 
 let task = readTask(`${__dirname}/fixtures/2017-07-15-lev/task.tsk`);
 let flight = readFlight(`${__dirname}/fixtures/2017-07-15-lev/SW_77flqgg1.igc`);
+let flight2 = readFlight(`${__dirname}/fixtures/2017-07-15-lev/IGP_77fg7sd1.igc`);
 let result = analyzeFlight(flight, task);
 
 console.log(formatResult(result));
 
 let json = taskToGeoJSON(task);
 json.features.push(turf.lineString(flight.map(it => it.coordinate), { color: 'red', opacity: 0.85 }));
+json.features.push(turf.lineString(flight2.map(it => it.coordinate), { color: 'green', opacity: 0.85 }));
 json.features.push(result.start.point);
 json.features.push(result.finish.point);
 

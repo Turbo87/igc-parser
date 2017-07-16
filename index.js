d0e8ecf5eafff47eacbe2cb0c604885ccca98e46
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-16 18:37:43 +0200

    Add flight to GeoJSON view

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const turf = require('@turf/turf');
+
 const readTask = require('./src/read-task');
 const taskToGeoJSON = require('./src/task-to-geojson');
+const readFlight = require('./src/read-flight');
 const viewGeoJSON = require('./src/view-geojson');
 
 let task = readTask(`${__dirname}/fixtures/2017-07-15-lev/task.tsk`);
+let flight = readFlight(`${__dirname}/fixtures/2017-07-15-lev/SW_77flqgg1.igc`);
+
 let json = taskToGeoJSON(task);
+json.features.push(turf.lineString(flight, { color: 'red', opacity: 0.85 }));
 
 viewGeoJSON(json);

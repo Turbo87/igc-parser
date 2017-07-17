7cb7e7763e65b04d5077da93609a69f9dfd8dad2
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-17 20:32:12 +0200

    examples: Convert to TypeScript

--- examples/show-flights.ts
+++ examples/show-flights.ts
@@ -1,12 +1,11 @@
-const fs = require('fs');
-const turf = require('@turf/turf');
+import * as turf from "@turf/turf";
 
-const readTask = require('../src/read-task').readTask;
-const taskToGeoJSON = require('../src/task-to-geojson').taskToGeoJSON;
-const readFlight = require('../src/read-flight').readFlight;
-const analyzeFlight = require('../src/analyze-flight').analyzeFlight;
-const formatResult = require('../src/format-result').formatResult;
-const viewGeoJSON = require('../src/view-geojson').viewGeoJSON;
+import {readTask} from "../src/read-task";
+import {taskToGeoJSON} from "../src/task-to-geojson";
+import {readFlight} from "../src/read-flight";
+import {analyzeFlight} from "../src/analyze-flight";
+import {formatResult} from "../src/format-result";
+import {viewGeoJSON} from "../src/view-geojson";
 
 let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev/task.tsk`);
 let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/sq_2017-07-15-fla-3jv-01.igc`);
@@ -21,5 +20,5 @@ json.features.push(turf.lineString(flight2.map(it => it.coordinate), { color: 'g
 json.features.push(...result.aatPoints.filter(Boolean).map(it => turf.point(it.coordinate)));
 
 if (process.argv.indexOf('--view') !== -1) {
-  viewGeoJSON(json);
+  viewGeoJSON(json, null);
 }

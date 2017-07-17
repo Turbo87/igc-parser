0ba2157fa3888bfb272c4a0a692aa0bf7e92d044
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 01:02:51 +0200

    Add second task file fixture

--- tests/xcsoar/task-reader.test.ts
+++ tests/xcsoar/task-reader.test.ts
@@ -6,5 +6,10 @@ describe('XCSoar - TaskReader - read()', () => {
   it('reads "2017-07-15-lev" task correctly', () => {
     let xml = fs.readFileSync(`${__dirname}/../../fixtures/2017-07-15-lev/task.tsk`, 'utf8');
     expect(read(xml)).toMatchSnapshot();
-  })
+  });
+
+  it('reads "2017-07-17-lev" task correctly', () => {
+    let xml = fs.readFileSync(`${__dirname}/../../fixtures/2017-07-17-lev.tsk`, 'utf8');
+    expect(read(xml)).toMatchSnapshot();
+  });
 });

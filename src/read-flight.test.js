0ac9d473abeab102ba33351bd18bfdb3b4652af3
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-17 18:16:40 +0200

    Fix test fixture path

--- src/read-flight.test.js
+++ src/read-flight.test.js
@@ -3,7 +3,7 @@ const readFlight = require('./read-flight');
 
 describe('readFlight()', () => {
   it(`reads "SW_77flqgg1" flight log correctly`, () => {
-    let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/SW_77flqgg1.igc`);
+    let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/SW_77flqgg1.IGC`);
     expect(flight).toMatchSnapshot();
   })
 });

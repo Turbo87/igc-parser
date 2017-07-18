8ea569aa06f1a345211d16c3fe304e981d7056c0
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 22:36:12 +0200

    Move task file

--- tests/task-to-geojson.test.ts
+++ tests/task-to-geojson.test.ts
@@ -3,7 +3,7 @@ import {taskToGeoJSON} from "../src/task-to-geojson";
 
 describe('taskToGeoJSON()', () => {
   it('converts "2017-07-15-lev" task correctly', () => {
-    let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev/task.tsk`);
+    let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev.tsk`);
     let geojson = taskToGeoJSON(task);
     expect(geojson).toMatchSnapshot();
   })
--- tests/xcsoar/index.test.ts
+++ tests/xcsoar/index.test.ts
@@ -4,7 +4,7 @@ import {read} from "../../src/xcsoar";
 
 describe('XCSoar - TaskReader - read()', () => {
   it('reads "2017-07-15-lev" task correctly', () => {
-    let xml = fs.readFileSync(`${__dirname}/../../fixtures/2017-07-15-lev/task.tsk`, 'utf8');
+    let xml = fs.readFileSync(`${__dirname}/../../fixtures/2017-07-15-lev.tsk`, 'utf8');
     expect(read(xml)).toMatchSnapshot();
   });
 

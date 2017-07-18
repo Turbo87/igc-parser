14a957a2a79580815a0552883629e527afaf8b53
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 22:52:12 +0200

    examples/calc-ranking: Accept task file and IGC files folder

--- examples/calc-ranking.ts
+++ examples/calc-ranking.ts
@@ -4,20 +4,51 @@ import {readTask} from "../src/read-task";
 import {readFlight} from "../src/read-flight";
 import {analyzeFlight} from "../src/analyze-flight";
 
-let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev/task.tsk`);
-
-let flights = fs.readdirSync(`${__dirname}/../fixtures/2017-07-15-lev`).filter(filename => (/\.igc$/i).test(filename)).map(filename => {
-  let callsign = filename.match(/^(.{1,3})_/)![1];
-  let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/${filename}`);
-  let result = analyzeFlight(flight, task);
-
-  return { filename, result, callsign };
-}).sort(compareSpeed);
-
-function compareSpeed(a: any, b: any) {
-  return b.result.speed - a.result.speed;
+if (process.argv.length < 4) {
+  console.log('Usage: ts-node examples/analyze-flight.ts TASK_PATH IGC_FOLDER');
+  process.exit(1);
 }
 
+let taskPath = process.argv[2];
+let task = readTask(taskPath);
+
+if (task.options.isAAT) {
+  console.log('AAT tasks are not supported yet');
+  process.exit(1);
+}
+
+let flightsPath = process.argv[3];
+let flights = fs.readdirSync(flightsPath)
+  .filter(filename => (/\.igc$/i).test(filename))
+  .map(filename => {
+    let callsign = filename.match(/^(.{1,3})_/)![1];
+    let flight = readFlight(`${flightsPath}/${filename}`);
+    let result = analyzeFlight(flight, task);
+
+    return { result, callsign };
+  })
+  .sort(compareFlights);
+
 flights.forEach(flight => {
-  console.log(`${flight.callsign}: ${flight.result.speed} km/h`);
+  let distance = flight.result.distance !== undefined ? `${(flight.result.distance / 1000).toFixed(1)} km` : '';
+  let speed = flight.result.speed !== undefined ? `${(flight.result.speed).toFixed(2)} km/h` : '';
+
+  console.log(`${flight.callsign}\t${distance}\t${speed}`);
 });
+
+function compareFlights(a: any, b: any) {
+  return compareResults(a.result, b.result);
+}
+
+function compareResults(a: any, b: any) {
+  if (a.speed !== undefined && b.speed !== undefined)
+    return b.speed - a.speed;
+
+  if (a.speed !== undefined && b.speed === undefined)
+    return -1;
+
+  if (a.speed === undefined && b.speed !== undefined)
+    return 1;
+
+  return b.distance - a.distance;
+}

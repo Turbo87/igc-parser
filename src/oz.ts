e974470e44ef54ba525a17faed742f489da2c69e
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 18:29:10 +0200

    Add "Point" type

--- src/oz.ts
+++ src/oz.ts
@@ -1,20 +1,22 @@
 import * as turf from "@turf/turf";
 import * as cheapRuler from "cheap-ruler";
 
+import Point from "./point";
+
 export interface ObservationZone {
-  center: cheapRuler.Point;
+  center: Point;
 
   checkEnter(c1: GeoJSON.Position, c2: GeoJSON.Position): GeoJSON.Feature<GeoJSON.Point> | undefined;
 }
 
 export class Cylinder implements ObservationZone {
-  center: cheapRuler.Point;
+  center: Point;
   radius: number;
 
   private readonly _ruler: cheapRuler.CheapRuler;
 
   constructor(center: GeoJSON.Position, radius: number) {
-    this.center = center as cheapRuler.Point;
+    this.center = center as Point;
     this.radius = radius;
     this._ruler = cheapRuler(center[1]);
   }
@@ -29,13 +31,13 @@ export class Cylinder implements ObservationZone {
   }
 
   isInside(coordinate: GeoJSON.Position) {
-    let distance = this._ruler.distance(coordinate as cheapRuler.Point, this.center);
+    let distance = this._ruler.distance(coordinate as Point, this.center);
     return distance <= this.radius / 1000;
   }
 }
 
 export class Line implements ObservationZone {
-  readonly center: cheapRuler.Point;
+  readonly center: Point;
   readonly length: number;
   readonly direction: number; // direction in which the line is triggering transitions
   readonly coordinates: cheapRuler.Line;
@@ -43,7 +45,7 @@ export class Line implements ObservationZone {
   private readonly _ruler: cheapRuler.CheapRuler;
 
   constructor(center: GeoJSON.Position, length: number, direction: number) {
-    this.center = center as cheapRuler.Point;
+    this.center = center as Point;
     this.length = length;
     this.direction = direction;
 
@@ -60,7 +62,7 @@ export class Line implements ObservationZone {
     if (intersection.features.length === 0)
       return;
 
-    let bearing = this._ruler.bearing(c1 as cheapRuler.Point, c2 as cheapRuler.Point);
+    let bearing = this._ruler.bearing(c1 as Point, c2 as Point);
     let bearingDiff = turf.bearingToAngle(this.direction - bearing);
     if (bearingDiff > 90 && bearingDiff < 270)
       return;

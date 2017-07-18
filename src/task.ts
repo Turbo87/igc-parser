98b7f8b8d009a624642707910b2727f5ac8ea019
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 20:59:16 +0200

    Task: Add _ruler property

--- src/task.ts
+++ src/task.ts
@@ -1,3 +1,6 @@
+import * as turf from '@turf/turf';
+import * as cheapRuler from "cheap-ruler";
+
 import {Turnpoint} from "./turnpoint";
 import {FinishPoint, StartPoint} from "./task-points";
 
@@ -8,12 +11,17 @@ export default class Task {
   start: StartPoint;
   finish: FinishPoint;
 
+  private readonly _ruler: cheapRuler.CheapRuler;
+
   constructor(points: Turnpoint[], options: TaskOptions) {
     this.points = points;
     this.options = options;
 
     this.start = new StartPoint(points[0].observationZone);
     this.finish = new FinishPoint(points[points.length - 1].observationZone);
+
+    let center = turf.center(turf.multiPoint(points.map(point => point.observationZone.center)));
+    this._ruler = cheapRuler(center.geometry.coordinates[1]);
   }
 }
 

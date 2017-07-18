2675f6f1e5546cb0cc10fcf690992543592090ff
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 22:09:23 +0200

    examples/show-flights: Show ZG flight from day 2

--- examples/show-flights.ts
+++ examples/show-flights.ts
@@ -4,20 +4,14 @@ import {readTask} from "../src/read-task";
 import {taskToGeoJSON} from "../src/task-to-geojson";
 import {readFlight} from "../src/read-flight";
 import {analyzeFlight} from "../src/analyze-flight";
-import {formatResult} from "../src/format-result";
 import {viewGeoJSON} from "./utils/view-geojson";
 
-let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev/task.tsk`);
-let flight = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/SW_77flqgg1.IGC`);
-let flight2 = readFlight(`${__dirname}/../fixtures/2017-07-15-lev/IGP_77fg7sd1.igc`);
+let task = readTask(`${__dirname}/../fixtures/2017-07-17-lev.tsk`);
+let flight = readFlight(`${__dirname}/../fixtures/2017-07-17-lev/ZG_77hv6ci1.igc`);
 let result = analyzeFlight(flight, task);
-
-console.log(formatResult(result));
 
 let json = taskToGeoJSON(task);
 json.features.push(turf.lineString(flight.map(it => it.coordinate), { color: 'red', opacity: 0.85 }));
-json.features.push(turf.lineString(flight2.map(it => it.coordinate), { color: 'green', opacity: 0.85 }));
-json.features.push(...result.aatPoints.filter(Boolean).map(it => turf.point(it.coordinate)));
 
 if (process.argv.indexOf('--view') !== -1) {
   viewGeoJSON(json);

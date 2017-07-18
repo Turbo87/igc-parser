0c44bacd1e35ed789c96528d41471f28d6ac4e98
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 18:07:30 +0200

    Remove pointless readTask() test

--- tests/read-task.test.ts
+++
@@ -1,8 +0,0 @@
-import {readTask} from "../src/read-task";
-
-describe('readTask()', () => {
-  it('reads "2017-07-15-lev" task correctly', () => {
-    let task = readTask(`${__dirname}/../fixtures/2017-07-15-lev/task.tsk`);
-    expect(task).toMatchSnapshot();
-  })
-});

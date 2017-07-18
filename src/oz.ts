a4864ac8b94dd35f615add0500eb15940e3b9940
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 18:12:52 +0200

    Use "cheap-ruler" for OZ calculations

--- src/oz.ts
+++ src/oz.ts
@@ -1,18 +1,22 @@
 import * as turf from "@turf/turf";
+import * as cheapRuler from "cheap-ruler";
 
 export interface ObservationZone {
-  center: GeoJSON.Position;
+  center: cheapRuler.Point;
 
   checkEnter(c1: GeoJSON.Position, c2: GeoJSON.Position): GeoJSON.Feature<GeoJSON.Point> | undefined;
 }
 
 export class Cylinder implements ObservationZone {
-  center: GeoJSON.Position;
+  center: cheapRuler.Point;
   radius: number;
 
+  private readonly _ruler: cheapRuler.CheapRuler;
+
   constructor(center: GeoJSON.Position, radius: number) {
-    this.center = center;
+    this.center = center as cheapRuler.Point;
     this.radius = radius;
+    this._ruler = cheapRuler(center[1]);
   }
 
   checkEnter(c1: GeoJSON.Position, c2: GeoJSON.Position): GeoJSON.Feature<GeoJSON.Point> | undefined {
@@ -25,26 +29,30 @@ export class Cylinder implements ObservationZone {
   }
 
   isInside(coordinate: GeoJSON.Position) {
-    let distance = turf.distance(coordinate, this.center);
+    let distance = this._ruler.distance(coordinate as cheapRuler.Point, this.center);
     return distance <= this.radius / 1000;
   }
 }
 
 export class Line implements ObservationZone {
-  readonly center: GeoJSON.Position;
+  readonly center: cheapRuler.Point;
   readonly length: number;
   readonly direction: number; // direction in which the line is triggering transitions
-  readonly coordinates: GeoJSON.Position[];
+  readonly coordinates: cheapRuler.Line;
+
+  private readonly _ruler: cheapRuler.CheapRuler;
 
   constructor(center: GeoJSON.Position, length: number, direction: number) {
-    this.center = center;
+    this.center = center as cheapRuler.Point;
     this.length = length;
     this.direction = direction;
 
-    let p1 = turf.destination(this.center, this.length / 2000, this.direction + 90);
-    let p2 = turf.destination(this.center, this.length / 2000, this.direction - 90);
+    this._ruler = cheapRuler(center[1]);
 
-    this.coordinates = [p1.geometry.coordinates, p2.geometry.coordinates];
+    let p1 = this._ruler.destination(this.center, this.length / 2000, this.direction + 90);
+    let p2 = this._ruler.destination(this.center, this.length / 2000, this.direction - 90);
+
+    this.coordinates = [p1, p2];
   }
 
   checkEnter(c1: GeoJSON.Position, c2: GeoJSON.Position): GeoJSON.Feature<GeoJSON.Point> | undefined {
@@ -52,7 +60,7 @@ export class Line implements ObservationZone {
     if (intersection.features.length === 0)
       return;
 
-    let bearing = turf.bearing(c1, c2);
+    let bearing = this._ruler.bearing(c1 as cheapRuler.Point, c2 as cheapRuler.Point);
     let bearingDiff = turf.bearingToAngle(this.direction - bearing);
     if (bearingDiff > 90 && bearingDiff < 270)
       return;

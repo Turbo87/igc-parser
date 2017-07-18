d746b974afbeb10a468518aa46203c7c187d844a
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-18 20:04:43 +0200

    taskToGeoJSON: Add support for Keyholes

--- src/task-to-geojson.ts
+++ src/task-to-geojson.ts
@@ -7,10 +7,22 @@ export function taskToGeoJSON(task: Task) {
   let legs = turf.lineString(task.points.map(pt => pt.observationZone.center));
 
   let ozs = task.points.map((pt, i) => {
-    if (pt.observationZone instanceof oz.Line) {
-      return turf.lineString(pt.observationZone.coordinates);
-    } else if (pt.observationZone instanceof oz.Cylinder) {
-      return turf.circle(pt.observationZone.center, pt.observationZone.radius / 1000);
+    let shape = pt.observationZone;
+
+    if (shape instanceof oz.Line) {
+      return turf.lineString(shape.coordinates);
+    } else if (shape instanceof oz.Cylinder) {
+      return turf.circle(shape.center, shape.radius / 1000);
+    } else if (shape instanceof oz.Keyhole) {
+      let circle = turf.circle(shape.center, shape.innerRadius / 1000);
+      let sector = turf.sector(
+        turf.point(shape.center),
+        shape.outerRadius / 1000,
+        shape.direction - shape.outerAngle / 2,
+        shape.direction + shape.outerAngle / 2
+      );
+
+      return turf.union(circle, sector);
     }
   }).filter(Boolean) as GeoJSON.Feature<any>[];
 

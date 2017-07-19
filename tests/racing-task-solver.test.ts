d64fa2327b8c4b0bbfa690405dc45a51d131edd5
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-20 00:22:47 +0200

    RacingTaskSolver: Remove event emitter tests

--- tests/racing-task-solver.test.ts
+++ tests/racing-task-solver.test.ts
@@ -1,49 +1,26 @@
 import {readTask} from "../src/read-task";
 import RacingTaskSolver from "../src/task/solver/racing-task-solver";
 import Task from "../src/task/task";
-import {Fix, readFlight} from "../src/read-flight";
-import {Event} from "../src/task/events";
+import {readFlight} from "../src/read-flight";
 
 describe('RacingTaskSolver', () => {
   describe('with task "2017-07-17-lev.tsk"', () => {
     let task: Task;
     let solver: RacingTaskSolver;
-    let flight: Fix[];
 
     beforeEach(() => {
       task = readTask(`${__dirname}/../fixtures/2017-07-17-lev.tsk`);
       solver = new RacingTaskSolver(task);
-      flight = readFlight(`${__dirname}/../fixtures/2017-07-17-lev/IGP_77hg7sd1.IGC`);
-    });
-
-    it('emits multiple start events', () => {
-      let events: Event[] = [];
-      solver.on('start', event => events.push(event));
-      solver.consume(flight);
-      expect(events).toMatchSnapshot();
-    });
-
-    it('emits multiple turn events', () => {
-      let events: Event[] = [];
-      solver.on('turn', (event) => events.push(event));
-      solver.consume(flight);
-      expect(events).toMatchSnapshot();
-    });
-
-    it('emits single finish event', () => {
-      let events: Event[] = [];
-      solver.on('finish', event => events.push(event));
-      solver.consume(flight);
-      expect(events).toMatchSnapshot();
     });
 
     it('returns a result', () => {
+      let flight = readFlight(`${__dirname}/../fixtures/2017-07-17-lev/IGP_77hg7sd1.IGC`);
       solver.consume(flight);
       expect(solver.result).toMatchSnapshot();
     });
 
     it('can handle outlandings', () => {
-      flight = readFlight(`${__dirname}/../fixtures/2017-07-17-lev/ZG_77hv6ci1.igc`);
+      let flight = readFlight(`${__dirname}/../fixtures/2017-07-17-lev/ZG_77hv6ci1.igc`);
       solver.consume(flight);
       expect(solver.result).toMatchSnapshot();
     })

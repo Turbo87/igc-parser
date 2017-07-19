2b13c7026e2631cfa2743b6de1c2cb179de455e2
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-20 01:40:19 +0200

    Fix "analyze-flight" example

--- examples/analyze-flight.ts
+++ examples/analyze-flight.ts
@@ -2,7 +2,7 @@ import {readTask} from "../src/read-task";
 import {readFlight} from "../src/read-flight";
 import RacingTaskSolver from "../src/task/solver/racing-task-solver";
 import {formatTime} from "../src/format-result";
-import {TurnEvent} from "../src/task/events";
+import {FinishEvent, StartEvent, TurnEvent} from "../src/task/events";
 
 if (process.argv.length < 4) {
   console.log('Usage: ts-node examples/analyze-flight.ts TASK_PATH IGC_PATH');
@@ -22,8 +22,15 @@ if (task.options.isAAT) {
 
 let solver = new RacingTaskSolver(task);
 
-solver.on('start', event => console.log(`Start at ${formatTime(event.time)}`));
-solver.on('turn', (event: TurnEvent) => console.log(`Reached TP${event.num} at ${formatTime(event.time)}`));
-solver.on('finish', event => console.log(`Finish at ${formatTime(event.time)}`));
-
 solver.consume(flight);
+
+solver.events.forEach(event => {
+  if (event instanceof StartEvent)
+    console.log(`Start at ${formatTime(event.time)}`);
+
+  if (event instanceof TurnEvent)
+    console.log(`Reached TP${event.num} at ${formatTime(event.time)}`);
+
+  if (event instanceof FinishEvent)
+    console.log(`Finish at ${formatTime(event.time)}`);
+});

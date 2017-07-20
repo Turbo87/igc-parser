e8f7aedf53e81a088e8a448de4b8bc9410634bcb
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-20 14:10:21 +0200

    OGNClient: Create new Socket for each connect() call

--- src/ogn/index.ts
+++ src/ogn/index.ts
@@ -1,7 +1,9 @@
 import {Socket} from "net";
 import * as readline from "readline";
+import {ReadLine} from "readline";
 
 const aprs = require('aprs-parser');
+const Emitter = require('tiny-emitter');
 
 export default class OGNClient {
   readonly host = 'aprs.glidernet.org';
@@ -14,49 +16,61 @@ export default class OGNClient {
 
   readonly senders: string[];
 
-  private readonly socket = new Socket();
-  private readonly reader = readline.createInterface({ input: this.socket as NodeJS.ReadableStream });
+  private socket: Socket | undefined;
+  private reader: ReadLine | undefined;
+  private readonly _emitter = new Emitter();
   private readonly parser = new aprs.APRSParser();
   private _timer: any;
 
   constructor(senders: string[]) {
     this.senders = senders;
-    this.reader.on('line', line => this.handleLine(line));
+  }
 
-    this.socket.on('close', () => {
+  connect() {
+    let socket = this.socket = new Socket();
+    let reader = this.reader = readline.createInterface({ input: socket as NodeJS.ReadableStream });
+
+    socket.connect(this.port, this.host, () => {
+      socket.write(`user ${this.user} pass ${this.pass} vers ${this.appName} ${this.appVersion}\n`);
+      this._emitter.emit('ready');
+    });
+
+    socket.on('close', () => {
       if (this._timer) {
         clearTimeout(this._timer);
         this._timer = undefined;
       }
+      this._emitter.emit('close');
     });
-  }
 
-  connect() {
-    this.socket.connect(this.port, this.host, () => {
-      this.socket.write(`user ${this.user} pass ${this.pass} vers ${this.appName} ${this.appVersion}\n`);
-      this.socket.emit('ready');
-    });
+    reader.on('line', line => this.handleLine(line));
 
     this._timer = setTimeout(() => this.sendKeepAlive(), 30000);
   }
 
   handleLine(line: string) {
+    this._emitter.emit('line', line);
+
     let knownSender = this.senders.some(sender => line.indexOf(sender) !== -1);
     if (knownSender) {
       let record = this.parser.parse(line);
-      this.socket.emit('record', record);
+      this._emitter.emit('record', record);
     }
+  }
+
+  scheduleKeepAlive() {
+    this._timer = setTimeout(() => {
+      this.sendKeepAlive();
+      this.scheduleKeepAlive();
+    }, 30000);
   }
 
   sendKeepAlive() {
-    this.socket.write('# keep alive');
+    if (this.socket)
+      this.socket.write('# keep alive');
   }
 
   on(event: string, handler: Function) {
-    if (event === 'line') {
-      this.reader.on.call(this.reader, event, handler);
-    } else {
-      this.socket.on.call(this.socket, event, handler);
-    }
+    this._emitter.on(event, handler);
   }
 }

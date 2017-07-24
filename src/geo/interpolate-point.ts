2ecf34d98cfd19b745be207205fd87aed6fb7006
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-24 19:33:33 +0200

    Fix interpolation functions
    🙈

--- src/geo/interpolate-point.ts
+++ src/geo/interpolate-point.ts
@@ -1,6 +1,8 @@
 import Point from "./point";
 
 export function interpolatePoint(p1: Point, p2: Point, fraction: number): Point {
-  let fraction2 = 1 - fraction;
-  return [p1[0] * fraction + p2[0] * fraction2, p1[1] * fraction + p2[1] * fraction2];
+  let fraction1 = 1 - fraction;
+  let fraction2 = fraction;
+
+  return [p1[0] * fraction1 + p2[0] * fraction2, p1[1] * fraction1 + p2[1] * fraction2];
 }
--- src/utils/interpolate-fix.ts
+++ src/utils/interpolate-fix.ts
@@ -2,13 +2,14 @@ import {Fix} from "../read-flight";
 import {interpolatePoint} from "../geo/interpolate-point";
 
 export function interpolateFix(fix1: Fix, fix2: Fix, fraction: number): Fix {
-  let fraction2 = 1 - fraction;
+  let fraction1 = 1 - fraction;
+  let fraction2 = fraction;
 
-  let time = fix1.time * fraction + fix2.time * fraction2;
+  let time = fix1.time * fraction1 + fix2.time * fraction2;
 
   let altitude;
   if (fix1.altitude !== undefined && fix2.altitude !== undefined) {
-    altitude = fix1.altitude * fraction + fix2.altitude * fraction2;
+    altitude = fix1.altitude * fraction1 + fix2.altitude * fraction2;
   }
 
   let valid = fix1.valid && fix2.valid;

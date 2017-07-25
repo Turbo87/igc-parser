24023d23abc8508a0effbe0e6d03a5e1d67061ee
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-25 19:51:52 +0200

    AreaTaskSolver: Use TaskPointTracker instance

--- src/task/solver/area-task-solver.ts
+++ src/task/solver/area-task-solver.ts
@@ -1,24 +1,15 @@
-import * as turf from "@turf/turf";
-
 import {Fix} from "../../read-flight";
 import Task from "../task";
-import AreaShape from "../shapes/area";
-import {interpolateFix} from "../../utils/interpolate-fix";
-
-const convexHull = require('convex-hull');
+import TaskPointTracker from "../task-point-tracker";
 
 export default class AreaTaskSolver {
   task: Task;
 
-  private _lastFix: Fix | undefined = undefined;
-  private _areas: Array<Fix[]> = [];
+  private _tracker: TaskPointTracker;
 
   constructor(task: Task) {
     this.task = task;
-
-    for (let i = 1; i < this.task.points.length - 1; i++) {
-      this._areas[i] = [];
-    }
+    this._tracker = new TaskPointTracker(task, { trackConvexHull: true });
   }
 
   consume(fixes: Fix[]) {
@@ -26,35 +17,10 @@ export default class AreaTaskSolver {
   }
 
   update(fix: Fix) {
-    if (this._lastFix) {
-      this._update(fix, this._lastFix);
-    }
-    this._lastFix = fix;
-  }
-
-  _update(fix: Fix, lastFix: Fix) {
-    for (let i = 1; i < this.task.points.length - 1; i++) {
-      let tp = this.task.points[i];
-      if (tp.shape instanceof AreaShape) {
-        if (tp.shape.isInside(fix.coordinate)) {
-          this._areas[i].push(fix);
-        }
-
-        let intersections = tp.shape.findIntersections(lastFix.coordinate, fix.coordinate);
-        this._areas[i].push(...intersections.map(fraction => interpolateFix(lastFix, fix, fraction)));
-      }
-    }
-  }
-
-  get thinnedAreas(): Array<Fix[]> {
-    return this._areas.map(fixes => {
-      let coords = fixes.map(fix => fix.coordinate);
-      let hull = convexHull(coords);
-      return hull.map((indices: any) => fixes[indices[0]]);
-    });
+    this._tracker.update(fix);
   }
 
   get result() {
-    return this.thinnedAreas.map(it => it.length);
+    return null;
   }
 }

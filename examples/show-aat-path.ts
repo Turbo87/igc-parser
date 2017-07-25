837e2e5d12da2cd4d6e315a9493107f7c0df2e58
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-25 22:32:10 +0200

    Add "show-aat-path" example

---
+++ examples/show-aat-path.ts
@@ -0,0 +1,35 @@
+import * as turf from "@turf/turf";
+
+import {readTask} from "../src/read-task";
+import {readFlight} from "../src/read-flight";
+import AreaTaskSolver from "../src/task/solver/area-task-solver";
+import {viewGeoJSON} from "./utils/view-geojson";
+import {taskToGeoJSON} from "../src/task-to-geojson";
+
+if (process.argv.length < 4) {
+  console.log('Usage: ts-node examples/show-aat-path.ts TASK_PATH IGC_PATH');
+  process.exit(1);
+}
+
+let taskPath = process.argv[2];
+let task = readTask(taskPath);
+
+if (!task.options.isAAT) {
+  console.log('Only AAT tasks are supported');
+  process.exit(1);
+}
+
+let flightPath = process.argv[3];
+let flight = readFlight(flightPath);
+
+let solver = new AreaTaskSolver(task);
+
+solver.consume(flight);
+
+let path = solver.result!.path;
+
+let json = taskToGeoJSON(task);
+json.features.push(turf.lineString(flight.map(it => it.coordinate), { color: 'red', opacity: 0.85 }));
+json.features.push(turf.lineString(path.map(it => it.coordinate), { color: '#00ff00', opacity: 0.85 }));
+
+viewGeoJSON(json);

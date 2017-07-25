cece0d2db7e405bbb217b1fe4ea2eeed9e4d4090
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-25 22:04:24 +0200

    AreaTaskSolver: Use "node-dijkstra" to solve turnpoint graph

--- src/task/solver/area-task-solver.ts
+++ src/task/solver/area-task-solver.ts
@@ -2,6 +2,8 @@ import {Fix} from "../../read-flight";
 import Task from "../task";
 import TaskPointTracker from "../task-point-tracker";
 
+const Graph = require('node-dijkstra')
+
 export default class AreaTaskSolver {
   task: Task;
 
@@ -28,39 +30,41 @@ export default class AreaTaskSolver {
       return areaVisits.map(visit => visit.fixes).reduce((array, fixes) => array.concat(fixes), []);
     });
 
-    let graph = Object.create(null);
+    let graph = new Graph();
 
     this._tracker.starts.forEach((start, i) => {
-      let startKey = `s-${i}`;
-      graph[startKey] = Object.create(null);
-
+      let edges: any = {};
       areaFixes[0].forEach((fix, j) => {
         let areaKey = `0-${j}`;
-        graph[startKey][areaKey] = -this.task.measureDistance(start.coordinate, fix.coordinate);
+        edges[areaKey] = 10000-this.task.measureDistance(start.coordinate, fix.coordinate);
       });
+
+      graph.addNode(`s-${i}`, edges);
     });
 
     areaFixes.forEach((fixes, i) => {
       if (i === areaFixes.length - 1) {
         fixes.forEach((fix, j) => {
-          let areaKey = `${i}-${j}`;
-          graph[areaKey] = Object.create(null);
-          graph[areaKey]['f'] = -this.task.measureDistance(fix.coordinate, this._tracker.finish!.coordinate);
+          graph.addNode(`${i}-${j}`, {
+            f: 10000-this.task.measureDistance(fix.coordinate, this._tracker.finish!.coordinate),
+          });
         });
       } else {
         let nextAreaFixes = areaFixes[i + 1];
         fixes.forEach((fix1, j) => {
-          let area1Key = `${i}-${j}`;
-          graph[area1Key] = Object.create(null);
-
+          let edges: any = {};
           nextAreaFixes.forEach((fix2, k) => {
             let area2Key = `${i + 1}-${k}`;
-            graph[area1Key][area2Key] = -this.task.measureDistance(fix1.coordinate, fix2.coordinate);
-          })
+            edges[area2Key] = 10000-this.task.measureDistance(fix1.coordinate, fix2.coordinate);
+          });
+
+          graph.addNode(`${i}-${j}`, edges);
         })
       }
     });
 
-    return graph;
+    let result = graph.path('s-0', 'f', { cost: true });
+
+    return { path: result.path, distance: (areaFixes.length + 1) * 10000 - result.cost };
   }
 }

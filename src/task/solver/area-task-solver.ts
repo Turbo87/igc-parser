11db6d1f3dfa754f6a0c7681dd3fa7a1166477ee
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-25 21:41:12 +0200

    AreaTaskSolver: Generate turnpoint graph

--- src/task/solver/area-task-solver.ts
+++ src/task/solver/area-task-solver.ts
@@ -21,6 +21,46 @@ export default class AreaTaskSolver {
   }
 
   get result() {
-    return null;
+    if (!this._tracker.hasFinish)
+      return null;
+
+    let areaFixes: Fix[][] = this._tracker.areaVisits.map(areaVisits => {
+      return areaVisits.map(visit => visit.fixes).reduce((array, fixes) => array.concat(fixes), []);
+    });
+
+    let graph = Object.create(null);
+
+    this._tracker.starts.forEach((start, i) => {
+      let startKey = `s-${i}`;
+      graph[startKey] = Object.create(null);
+
+      areaFixes[0].forEach((fix, j) => {
+        let areaKey = `0-${j}`;
+        graph[startKey][areaKey] = -this.task.measureDistance(start.coordinate, fix.coordinate);
+      });
+    });
+
+    areaFixes.forEach((fixes, i) => {
+      if (i === areaFixes.length - 1) {
+        fixes.forEach((fix, j) => {
+          let areaKey = `${i}-${j}`;
+          graph[areaKey] = Object.create(null);
+          graph[areaKey]['f'] = -this.task.measureDistance(fix.coordinate, this._tracker.finish!.coordinate);
+        });
+      } else {
+        let nextAreaFixes = areaFixes[i + 1];
+        fixes.forEach((fix1, j) => {
+          let area1Key = `${i}-${j}`;
+          graph[area1Key] = Object.create(null);
+
+          nextAreaFixes.forEach((fix2, k) => {
+            let area2Key = `${i + 1}-${k}`;
+            graph[area1Key][area2Key] = -this.task.measureDistance(fix1.coordinate, fix2.coordinate);
+          })
+        })
+      }
+    });
+
+    return graph;
   }
 }

f3b1246e6814478cb42e510fb3ba6ec65d0c1c3c
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-31 19:17:23 +0200

    examples/read-igc-files: Add IGCFilenameParser output

--- examples/read-igc-files.ts
+++ examples/read-igc-files.ts
@@ -1,4 +1,5 @@
 import IGCParser, {IGCFile} from '../src/igc/parser';
+import * as IGCFilenameParser from "../src/igc/filename-parser";
 
 import fs = require('fs');
 
@@ -13,20 +14,50 @@ let flightsPath = process.argv[2];
 let filenames = fs.readdirSync(flightsPath).filter(filename => (/\.igc$/i).test(filename));
 
 for (let filename of filenames) {
+  let filenameResult = IGCFilenameParser.parse(filename);
+
   let path = `${flightsPath}/${filename}`;
   let content = fs.readFileSync(path, 'utf8');
   let result = IGCParser.parse(content);
 
-  print(filename, result);
+  print(filename, filenameResult, result);
 }
 
 if (filenames.length > 0) {
   console.log(SEPARATOR);
 }
 
-function print(filename: string, result: IGCFile) {
+function print(filename: string, filenameResult: IGCFilenameParser.IGCFilenameData | null, result: IGCFile) {
   console.log(SEPARATOR);
-  console.log(`Filename:  ${filename}`);
-  console.log(`Date:      ${result.date}`);
-  console.log(`GPS fixes: ${result.fixes.length}`);
+  console.log();
+
+  printLine('Filename', filename);
+  console.log();
+
+  if (filenameResult) {
+    printLine('> Callsign', filenameResult.callsign);
+    printLine('> Date', filenameResult.date);
+    printLine('> Manufacturer', filenameResult.manufacturer);
+    printLine('> Logger ID', filenameResult.loggerId);
+    printLine('> Flight #', filenameResult.numFlight);
+    console.log();
+  }
+
+  printLine('Date', result.date);
+  printLine('GPS fixes', result.fixes.length);
+
+  console.log();
+}
+
+function printLine(title: string, value: any) {
+  const MIN_TITLE_LENGTH = 18;
+
+  title += ':';
+  while (title.length < MIN_TITLE_LENGTH) {
+    title += ' ';
+  }
+
+  if (value !== undefined && value !== null) {
+    console.log(`${title}${value}`);
+  }
 }

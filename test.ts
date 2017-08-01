afa407542bd60e23f9eaff41b4539db82074b51e
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-08-01 23:50:13 +0200

    Add test suite generator

--- test.ts
+++ test.ts
@@ -79,173 +79,176 @@ describe('IGCParser', () => {
     });
   });
 
-  describe('parseARecord()', () => {
-    it('parses valid HFDTE headers', () => {
-      expect(parser['parseARecord']('AFLA1BG')).toEqual({
-        manufacturer: 'Flarm',
-        loggerId: '1BG',
-        numFlight: null,
-        additionalData: null,
-      });
-
-      expect(parser['parseARecord']('AMMMNNN:TEXT STRING')).toEqual({
-        manufacturer: 'MMM',
-        loggerId: 'NNN',
-        numFlight: null,
-        additionalData: 'TEXT STRING',
-      });
-
-      expect(parser['parseARecord']('ALXV6M7FLIGHT:1')).toEqual({
-        manufacturer: 'LXNAV',
-        loggerId: '6M7',
-        numFlight: 1,
-        additionalData: null,
-      });
-
-      expect(parser['parseARecord']('ALXNNE2FLIGHT:42')).toEqual({
-        manufacturer: 'LX Navigation',
-        loggerId: 'NE2',
-        numFlight: 42,
-        additionalData: null,
-      });
+  describeMethod('parseARecord', (test) => {
+    test.pass('AFLA1BG', {
+      manufacturer: 'Flarm',
+      loggerId: '1BG',
+      numFlight: null,
+      additionalData: null,
     });
 
-    it('throws for invalid records', () => {
-      expect(() => parser['parseARecord']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseARecord']('HFDTE')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseARecord']('HFDTEXXXXXX')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseARecord']('HFDTE1234?6')).toThrowErrorMatchingSnapshot();
+    test.pass('AMMMNNN:TEXT STRING', {
+      manufacturer: 'MMM',
+      loggerId: 'NNN',
+      numFlight: null,
+      additionalData: 'TEXT STRING',
     });
+
+    test.pass('ALXV6M7FLIGHT:1', {
+      manufacturer: 'LXNAV',
+      loggerId: '6M7',
+      numFlight: 1,
+      additionalData: null,
+    });
+
+    test.pass('ALXNNE2FLIGHT:42', {
+      manufacturer: 'LX Navigation',
+      loggerId: 'NE2',
+      numFlight: 42,
+      additionalData: null,
+    });
+
+    test.fail('');
+    test.fail('HFDTE');
+    test.fail('HFDTEXXXXXX');
+    test.fail('HFDTE1234?6');
   });
 
-  describe('parseDateHeader()', () => {
-    it('parses valid HFDTE headers', () => {
-      expect(parser['parseDateHeader']('HFDTE010180')).toEqual('1980-01-01');
-      expect(parser['parseDateHeader']('HFDTE150717')).toEqual('2017-07-15');
-      expect(parser['parseDateHeader']('HFDTE311279')).toEqual('2079-12-31');
-    });
+  describeMethod('parseDateHeader', (test) => {
+    test.pass('HFDTE010180', '1980-01-01');
+    test.pass('HFDTE150717', '2017-07-15');
+    test.pass('HFDTE311279', '2079-12-31');
 
-    it('throws for invalid records', () => {
-      expect(() => parser['parseDateHeader']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseDateHeader']('HFDTE')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseDateHeader']('HFDTEXXXXXX')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseDateHeader']('HFDTE1234?6')).toThrowErrorMatchingSnapshot();
-    });
+    test.fail('');
+    test.fail('HFDTE');
+    test.fail('HFDTEXXXXXX');
+    test.fail('HFDTE1234?6');
   });
 
-  describe('parsePilot()', () => {
-    it('parses valid PLT headers', () => {
-      expect(parser['parsePilot']('HFPLTPILOT:')).toEqual('');
-      expect(parser['parsePilot']('HFPLTPilotincharge:Mike Havener')).toEqual('Mike Havener');
-      expect(parser['parsePilot']('HFPLTPILOT:BEN BRAND')).toEqual('BEN BRAND');
-      expect(parser['parsePilot']('HFPLTPILOT:foo:bar')).toEqual('foo:bar');
-      expect(parser['parsePilot']('HFPLTPILOTINCHARGE:YUTA_ISHIGURO')).toEqual('YUTA ISHIGURO');
-      expect(parser['parsePilot']('HFPLT Pilot             :Gisela Weinreich  ')).toEqual('Gisela Weinreich');
-      expect(parser['parsePilot']('HFPLT:John Doe')).toEqual('John Doe');
-      expect(parser['parsePilot']('HOPLTJohn Doe')).toEqual('John Doe');
-    });
+  describeMethod('parsePilot', (test) => {
+    test.pass('HFPLTPILOT:', '');
+    test.pass('HFPLTPilotincharge:Mike Havener', 'Mike Havener');
+    test.pass('HFPLTPILOT:BEN BRAND', 'BEN BRAND');
+    test.pass('HFPLTPILOT:foo:bar', 'foo:bar');
+    test.pass('HFPLTPILOTINCHARGE:YUTA_ISHIGURO', 'YUTA ISHIGURO');
+    test.pass('HFPLT Pilot             :Gisela Weinreich  ', 'Gisela Weinreich');
+    test.pass('HFPLT:John Doe', 'John Doe');
+    test.pass('HOPLTJohn Doe', 'John Doe');
 
-    it('throws for invalid records', () => {
-      expect(() => parser['parsePilot']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parsePilot']('HXPLT:John Doe')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parsePilot']('HFPLX:John Doe')).toThrowErrorMatchingSnapshot();
-    });
+    test.fail('');
+    test.fail('HXPLT:John Doe');
+    test.fail('HFPLX:John Doe');
   });
 
-  describe('parseCopilot()', () => {
-    it('parses valid CM2 headers', () => {
-      expect(parser['parseCopilot']('HFCM2CREW2:')).toEqual('');
-      expect(parser['parseCopilot']('HFCM2Crew 2:Mike Havener')).toEqual('Mike Havener');
-      expect(parser['parseCopilot']('HFCM2CREW2:BEN BRAND')).toEqual('BEN BRAND');
-      expect(parser['parseCopilot']('HFCM2CREW2:foo:bar')).toEqual('foo:bar');
-      expect(parser['parseCopilot']('HFCM2CREW2:YUTA_ISHIGURO')).toEqual('YUTA ISHIGURO');
-      expect(parser['parseCopilot']('HFCM2 Copilot           :Gisela Weinreich  ')).toEqual('Gisela Weinreich');
-      expect(parser['parseCopilot']('HFCM2:John Doe')).toEqual('John Doe');
-      expect(parser['parseCopilot']('HOCM2John Doe')).toEqual('John Doe');
+  describeMethod('parseCopilot', (test) => {
+    test.pass('HFCM2CREW2:', '');
+    test.pass('HFCM2Crew 2:Mike Havener', 'Mike Havener');
+    test.pass('HFCM2CREW2:BEN BRAND', 'BEN BRAND');
+    test.pass('HFCM2CREW2:foo:bar', 'foo:bar');
+    test.pass('HFCM2CREW2:YUTA_ISHIGURO', 'YUTA ISHIGURO');
+    test.pass('HFCM2 Copilot           :Gisela Weinreich  ', 'Gisela Weinreich');
+    test.pass('HFCM2:John Doe', 'John Doe');
+    test.pass('HOCM2John Doe', 'John Doe');
+
+    test.fail('');
+    test.fail('HXCM2:John Doe');
+    test.fail('HFCM1:John Doe');
+  });
+
+  describeMethod('parseBRecord', (test) => {
+    beforeEach(() => {
+      parser['_result'].date = '2017-02-03';
     });
 
-    it('throws for invalid records', () => {
-      expect(() => parser['parseCopilot']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseCopilot']('HXCM2:John Doe')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseCopilot']('HFCM1:John Doe')).toThrowErrorMatchingSnapshot();
+    test.pass('B1026555103888N00703115EA0065700751', {
+      timestamp: 1486117615000,
+      time: '10:26:55',
+      latitude: 51.0648,
+      longitude: 7.051916666666667,
+      valid: true,
+      pressureAltitude: 657,
+      gpsAltitude: 751,
+      extensions: {},
+      fixAccuracy: null,
+      enl: null,
     });
+
+    test.pass('B0000005103888N00703115EV0000000000', {
+      timestamp: 1486080000000,
+      time: '00:00:00',
+      latitude: 51.0648,
+      longitude: 7.051916666666667,
+      valid: false,
+      pressureAltitude: null,
+      gpsAltitude: null,
+      extensions: {},
+      fixAccuracy: null,
+      enl: null,
+    });
+
+    test.fail('');
+    test.fail('B10a6555103888N00703115EA0065700751');
+    test.fail('B1026555103888X00703115EA0065700751');
+    test.fail('B102655510Z888N00703115EA0065700751');
+    test.fail('B1026555103888N00703115NA0065700751');
   });
 
   describe('parseBRecord()', () => {
-    it('parses valid B records', () => {
-      parser['_result'].date = '2017-02-03';
-
-      expect(parser['parseBRecord']('B1026555103888N00703115EA0065700751')).toEqual({
-        timestamp: 1486117615000,
-        time: '10:26:55',
-        latitude: 51.0648,
-        longitude: 7.051916666666667,
-        valid: true,
-        pressureAltitude: 657,
-        gpsAltitude: 751,
-        extensions: {},
-        fixAccuracy: null,
-        enl: null,
-      });
-
-      expect(parser['parseBRecord']('B0000005103888N00703115EV0000000000')).toEqual({
-        timestamp: 1486080000000,
-        time: '00:00:00',
-        latitude: 51.0648,
-        longitude: 7.051916666666667,
-        valid: false,
-        pressureAltitude: null,
-        gpsAltitude: null,
-        extensions: {},
-        fixAccuracy: null,
-        enl: null,
-      });
-    });
-
     it('throws for missing "dateHeader"', () => {
       expect(() => parser['parseBRecord']('B1026555103888N00703115EA0065700751')).toThrowErrorMatchingSnapshot();
     });
-
-    it('throws for invalid records', () => {
-      parser['_result'].date = '2017-02-03';
-
-      expect(() => parser['parseBRecord']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseBRecord']('B10a6555103888N00703115EA0065700751')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseBRecord']('B1026555103888X00703115EA0065700751')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseBRecord']('B102655510Z888N00703115EA0065700751')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseBRecord']('B1026555103888N00703115NA0065700751')).toThrowErrorMatchingSnapshot();
-    });
   });
 
-  describe('parseIRecord()', () => {
-    it('parses valid I records', () => {
-      expect(parser['parseIRecord']('I013638FXA')).toEqual([
-        { code: 'FXA', start: 36, length: 3 },
-      ]);
+  describeMethod('parseIRecord', (test) => {
+    test.pass('I013638FXA', [
+      { code: 'FXA', start: 36, length: 3 },
+    ]);
 
-      expect(parser['parseIRecord']('I043638FXA3940SIU4143ENL4446MOP')).toEqual([
-        { code: 'FXA', start: 36, length: 3 },
-        { code: 'SIU', start: 39, length: 2 },
-        { code: 'ENL', start: 41, length: 3 },
-        { code: 'MOP', start: 44, length: 3 },
-      ]);
+    test.pass('I043638FXA3940SIU4143ENL4446MOP', [
+      { code: 'FXA', start: 36, length: 3 },
+      { code: 'SIU', start: 39, length: 2 },
+      { code: 'ENL', start: 41, length: 3 },
+      { code: 'MOP', start: 44, length: 3 },
+    ]);
 
-      expect(parser['parseIRecord']('I083638FXA3941ENL4246TAS4751GSP5254TRT5559VAT6063OAT6467ACZ')).toEqual([
-        { code: 'FXA', start: 36, length: 3 },
-        { code: 'ENL', start: 39, length: 3 },
-        { code: 'TAS', start: 42, length: 5 },
-        { code: 'GSP', start: 47, length: 5 },
-        { code: 'TRT', start: 52, length: 3 },
-        { code: 'VAT', start: 55, length: 5 },
-        { code: 'OAT', start: 60, length: 4 },
-        { code: 'ACZ', start: 64, length: 4 },
-      ]);
-    });
+    test.pass('I083638FXA3941ENL4246TAS4751GSP5254TRT5559VAT6063OAT6467ACZ', [
+      { code: 'FXA', start: 36, length: 3 },
+      { code: 'ENL', start: 39, length: 3 },
+      { code: 'TAS', start: 42, length: 5 },
+      { code: 'GSP', start: 47, length: 5 },
+      { code: 'TRT', start: 52, length: 3 },
+      { code: 'VAT', start: 55, length: 5 },
+      { code: 'OAT', start: 60, length: 4 },
+      { code: 'ACZ', start: 64, length: 4 },
+    ]);
 
-    it('throws for invalid records', () => {
-      expect(() => parser['parseIRecord']('')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseIRecord']('I023638FXA')).toThrowErrorMatchingSnapshot();
-      expect(() => parser['parseIRecord']('I0136FXA38')).toThrowErrorMatchingSnapshot();
-    });
+    test.fail('');
+    test.fail('I023638FXA');
+    test.fail('I0136FXA38');
   });
+
+  // Test Suite Generator
+
+  function describeMethod(methodName: string, cb: (test: any) => void) {
+    describe(`${methodName}()`, () => {
+      cb({
+        pass(input: string, expected: any) {
+          let testName = input ? input : '[empty]';
+          if (typeof expected === 'string') {
+            testName += ` -> ${expected ? `'${expected}'` : '[empty]'}`;
+          }
+
+          test(testName, () => {
+            expect(parser[methodName](input)).toEqual(expected);
+          });
+        },
+
+        fail(input: string) {
+          test(`${input ? input : '[empty]'} -> 💥 `, () => {
+            expect(() => parser[methodName](input)).toThrowErrorMatchingSnapshot();
+          });
+        },
+      });
+    });
+  }
 });
